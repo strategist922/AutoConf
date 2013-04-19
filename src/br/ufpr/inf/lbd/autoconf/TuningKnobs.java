67f1d64bdbac33048cde1fbee6a0f11689cc2b7d
Edson Ramiro Lucas Filho <erlfilho@gmail.com>
2013-04-19 16:01:53 -0300

    Added three new vars

--- src/br/ufpr/inf/lbd/autoconf/TuningKnobs.java
+++ src/br/ufpr/inf/lbd/autoconf/TuningKnobs.java
@@ -19,6 +19,9 @@
 package br.ufpr.inf.lbd.autoconf;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.Reducer;
 
 import java.io.Serializable;
 import java.util.Enumeration;
@@ -27,44 +30,43 @@ import java.util.Map;
 import java.util.Properties;
 
 public class TuningKnobs implements Serializable {
-  String jobName = "no-name";
-  Properties resources = new Properties();
-
+  String jobName = "unknown";
+  String jarFile = "unknown";
+  Properties knobs = new Properties();
+  Class<? extends Mapper> mapperClass = null;
+  Class<? extends Reducer> reducerClass = null;
 
   public TuningKnobs(Configuration conf) {
-    // TODO: conf.get("mapred.job.name") is not working :(
-    Iterator i = conf.iterator();
-    while (i.hasNext()) {
-      Map.Entry<String, String> p = (Map.Entry<String, String>) i.next();
-      if (p.getKey().matches("mapred.job.name")) {
-        setJobName(p.getValue());
-      }
-    }
-    applyKnobs(conf);
+    /* Jar file */
+    JobConf j = (JobConf) conf;
+    System.out.println("-->>> "+ j.getJar());
+    setJarFile(j.getJar());
+    
+    /* Mapper Class */
+    setMapperName(j.getMapperClass());
+    
+    /* Reducer Class */
+    setReducerName(j.getReducerClass());
+
+    /* Job Name */
+    setJobName(j.getJobName());
+
+    /* Copy the tuning knobs from conf to this.knobs */
+    saveTuningKnobs(conf);
   }
 
   public TuningKnobs(String name, Configuration conf) {
     setJobName(name);
-    applyKnobs(conf);
+    saveTuningKnobs(conf);
   }
 
   public TuningKnobs(String name, Properties properties) {
     setJobName(name);
-    resources = properties;
+    knobs = properties;
   }
 
-  private void applyKnobs(Configuration conf) {
-    System.out.println("AutoConf: There are " + conf.size() + " knobs available.");
-    Iterator i = conf.iterator();
-    while (i.hasNext()) {
-      Map.Entry<String, String> p = (Map.Entry<String, String>) i.next();
-      if (resources.getProperty(p.getKey()) != null)
-         resources.setProperty(p.getKey(), p.getValue());
-    }
-  }
-
-  public Properties getResources() {
-    return this.resources;
+  public Properties getKnobs() {
+    return this.knobs;
   }
 
   public void setJobName(String name) {
@@ -73,30 +75,75 @@ public class TuningKnobs implements Serializable {
     }
   }
 
+  private void setMapperName(Class<? extends Mapper> mapperClass) {
+    this.mapperClass = mapperClass;
+  }
+
+  public Class<? extends Mapper> getMapperName() {
+    return this.mapperClass;
+  }
+
+  private void setReducerName(Class<? extends Reducer> reducerClass) {
+    this.reducerClass = reducerClass;
+  }
+
+  public Class<? extends Reducer> getReducerName() {
+    return this.reducerClass;
+  }
+
   public String getJobName() {
     return this.jobName;
   }
 
+  public void setJarFile(String j) {
+    this.jarFile = j;
+  }
+
+  public String getJarFile() {
+    return this.jarFile;
+  }
+
+  private void saveTuningKnobs(Configuration conf) {
+    System.out.println("AutoConf: There are " + conf.size() + " knobs available.");
+    Iterator i = conf.iterator();
+    while (i.hasNext()) {
+      Map.Entry<String, String> p = (Map.Entry<String, String>) i.next();
+      if (knobs.getProperty(p.getKey()) != null)
+        knobs.setProperty(p.getKey(), p.getValue());
+    }
+  }
+
   public void copyResourcesTo(Configuration conf) {
-    if (resources.size() > 0) {
-      System.out.println("AutoConf: Applying " + resources.size() + " tuned knobs from " + conf.size() + " knobs.");
+    if (knobs.size() > 0) {
+      System.out.println("AutoConf: Applying " + knobs.size() + " tuned knobs from " + conf.size() + " knobs.");
       Iterator i = conf.iterator();
       while (i.hasNext()) {
         Map.Entry<String, String> p = (Map.Entry<String, String>) i.next();
-        if (resources.getProperty(p.getKey()) != null) {
-          conf.set(p.getKey(), resources.getProperty(p.getKey()));
-//          System.out.println(" *** " + p.getKey() + " :: " + resources.getProperty(p.getKey()));
+        if (knobs.getProperty(p.getKey()) != null) {
+          conf.set(p.getKey(), knobs.getProperty(p.getKey()));
+//          System.out.println(" *** " + p.getKey() + " :: " + knobs.getProperty(p.getKey()));
         }
       }
     }
   }
 
   public void showConfiguration () {
-    System.out.println("AutoConf: " + jobName + " has " + resources.size());
-    Enumeration e = resources.propertyNames();
+    System.out.println(" *** JobName :: " + getJobName());
+    System.out.println(" *** Knobs   :: " + getKnobs().size());
+    System.out.println(" *** JarFile :: " + getJarFile() );
+
+    if (getMapperName() != null)
+      System.out.println(" *** Mapper  :: " + getMapperName().getName());
+    if (getReducerName() != null)
+      System.out.println(" *** Reducer :: " + getReducerName().getName());
+
+    /* show the tuning knobs */
+    System.out.println(" *** Tuning Knobs");
+    Enumeration e = knobs.propertyNames();
     while (e.hasMoreElements()) {
       String key = (String) e.nextElement();
-      System.out.println(" --> " + key + " :: " + resources.getProperty(key));
+      System.out.println("    " + key + " = " + knobs.getProperty(key));
     }
+
   }
 }
