cddc04e8dde72a6688deac198877128356830b0c
Edson Ramiro Lucas Filho <erlfilho@gmail.com>
2013-04-30 16:41:02 -0300

    FeatureVector: Added basic Feature Vector representation

---
+++ src/br/ufpr/inf/lbd/autoconf/FeatureVector.java
@@ -0,0 +1,53 @@
+package br.ufpr.inf.lbd.autoconf;
+
+/**
+ * User: Edson
+ * Date: 4/30/13
+ * Time: 4:37 PM
+ */
+public class FeatureVector {
+
+  int cpu;
+  int mem;
+  int net;
+  int disk;
+
+  public void FeatureVector () {
+    setCpu(0);
+    setDisk(0);
+    setMem(0);
+    setDisk(0);
+  }
+
+  public int getCpu() {
+    return cpu;
+  }
+
+  public void setCpu(int cpu) {
+    this.cpu = cpu;
+  }
+
+  public int getMem() {
+    return mem;
+  }
+
+  public void setMem(int mem) {
+    this.mem = mem;
+  }
+
+  public int getNet() {
+    return net;
+  }
+
+  public void setNet(int net) {
+    this.net = net;
+  }
+
+  public int getDisk() {
+    return disk;
+  }
+
+  public void setDisk(int disk) {
+    this.disk = disk;
+  }
+}
