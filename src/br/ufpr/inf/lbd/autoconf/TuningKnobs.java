d11d0b51cec260e442087ed1467ff8491333fa36
Edson Ramiro Lucas Filho <erlfilho@gmail.com>
2013-07-09 17:24:14 -0300

    TuningKnobs.java: Several changes

--- src/br/ufpr/inf/lbd/autoconf/TuningKnobs.java
+++ src/br/ufpr/inf/lbd/autoconf/TuningKnobs.java
@@ -19,167 +19,59 @@
 package br.ufpr.inf.lbd.autoconf;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.FileUtil;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.Mapper;
-import org.apache.hadoop.mapred.Reducer;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.Serializable;
-import java.lang.reflect.Method;
-import java.util.*;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
 
+/**
+ *
+ */
 public class TuningKnobs implements Serializable {
-  String jobName = "unknown";
-  String jarFile = "unknown";
-  long inputSize = 0;
-  HashMap<String, Long> InputData = new HashMap<String, Long>();
   Properties knobs = new Properties();
-  Class<? extends Mapper> mapperClass = null;
-  Class<? extends Reducer> reducerClass = null;
-  FeatureVector features = new FeatureVector();
 
+  /**
+   *
+   * @param conf
+   * @throws IOException
+   */
   public TuningKnobs(Configuration conf) throws IOException {
-    /* Jar file */
-    JobConf j = (JobConf) conf;
-    setJarFile(j.getJar());
-
-    /* Input Size */
-    setInputSize(conf);
-
-    /* Mapper Class */
-    setMapperName(j.getMapperClass());
-
-    /* Reducer Class */
-    setReducerName(j.getReducerClass());
-
-    /* Job Name */
-    setJobName(j.getJobName());
-
-    /* Copy the tuning knobs from conf to this.knobs */
-    saveTuningKnobs(conf);
-
+    setTuningKnobs(conf);
   }
 
-  public TuningKnobs(String name, Configuration conf) {
-    setJobName(name);
-    saveTuningKnobs(conf);
-  }
-
-  public TuningKnobs(String name, Properties properties) {
-    setJobName(name);
-    knobs = properties;
-  }
-
-  public Properties getKnobs() {
+  /**
+   * Get the tuning knobs
+   * @return
+   */
+  public Properties getTuningKnobs() {
     return this.knobs;
   }
 
-  public void setJobName(String name) {
-    if (name != null) {
-      this.jobName = name;
-    }
-  }
-
-  private void setMapperName(Class<? extends Mapper> mapperClass) {
-    this.mapperClass = mapperClass;
-  }
-
-  public Class<? extends Mapper> getMapperName() {
-    return this.mapperClass;
-  }
-
-  private void setReducerName(Class<? extends Reducer> reducerClass) {
-    this.reducerClass = reducerClass;
-  }
-
-  public Class<? extends Reducer> getReducerName() {
-    return this.reducerClass;
-  }
-
-  public String getJobName() {
-    return this.jobName;
-  }
-
-  public void setJarFile(String j) {
-    this.jarFile = j;
-  }
-
-  public String getJarFile() {
-    return this.jarFile;
-  }
-
-  public HashMap<String, Long> getInputData() {
-    return InputData;
-  }
-
-  public long getInputSize() {
-    return inputSize;
-  }
-
-  public void setInputSize(long inputSize) {
-    this.inputSize = inputSize;
-  }
-
-
-  void setInputSize(Configuration conf) throws IOException {
-    String src = "";
-    Path paths[] = FileInputFormat.getInputPaths((JobConf)conf);
-    for (Path p : paths) {
-      String d[] = p.toString().split("//");
-      if (d.length<1) return;
-
-      String dirs[] = d[1].split("/");
-      if (dirs.length<1) return;
-
-      for (int i=1; i<dirs.length; i++) {
-        src = src + "/" + dirs[i];
+  /**
+   * Copy the Tuning Knobs from the Configuration conf
+   * to the local this.knobs
+   * @param path
+   */
+  public void loadKnobsFromFile(String path) {
+    try {
+      FileInputStream inputStream = new FileInputStream(path);
+      knobs.load(inputStream);
+      inputStream.close();
+      } catch (IOException e) {
+        e.printStackTrace();
       }
-    }
-
-    if (src.isEmpty()) {
-       return;
-    }
-
-    Path srcPath = new Path(src);
-    FileSystem srcFs = srcPath.getFileSystem(conf);
-    Path[] pathItems = FileUtil.stat2Paths(srcFs.globStatus(srcPath), srcPath);
-    FileStatus items[] = srcFs.listStatus(pathItems);
-
-    if ((items == null) || ((items.length == 0) && (!srcFs.exists(srcPath)))) {
-      throw new FileNotFoundException("Cannot access " + src + ": No such file or directory.");
-    } else {
-      long length[] = new long[items.length];
-      for (int i = 0; i < items.length; i++) {
-        length[i] = items[i].isDir() ? srcFs.getContentSummary(items[i].getPath()).getLength() : items[i].getLen();
-        int len = String.valueOf(length[i]).length();
-      }
-
-      for (int i = 0; i < items.length; i++) {
-        InputData.put(items[i].getPath().toString(), length[i]);
-        inputSize += length[i];
-      }
-    }
   }
 
-  private void saveTuningKnobs(Configuration conf) {
-    System.out.println("AutoConf: There are " + conf.size() + " knobs available.");
+  /**
+   * Copy the Tuning Knobs from the Configuration conf
+   * to the local this.knobs
+   * @param conf
+   */
+  private void setTuningKnobs(Configuration conf) {
     Iterator i = conf.iterator();
     while (i.hasNext()) {
       Map.Entry<String, String> p = (Map.Entry<String, String>) i.next();
@@ -188,41 +80,32 @@ public class TuningKnobs implements Serializable {
     }
   }
 
-  public void copyResourcesTo(Configuration conf) {
+  /**
+   * Copy the Tuning knobs from this.knobs
+   * to the Configuration conf of the job.
+   * @param conf
+   */
+  public void applyTuning(Configuration conf) {
+    System.out.println("AutoConf: Tuning job: ");
     if (knobs.size() > 0) {
-      System.out.println("AutoConf: Applying " + knobs.size() + " tuned knobs from " + conf.size() + " knobs.");
       for (Object aConf : conf) {
         Map.Entry<String, String> p = (Map.Entry<String, String>) aConf;
         if (knobs.getProperty(p.getKey()) != null) {
           conf.set(p.getKey(), knobs.getProperty(p.getKey()));
-//          System.out.println(" *** " + p.getKey() + " :: " + knobs.getProperty(p.getKey()));
+          System.out.println("\t - " + p.getKey() + " from " + p.getValue() + " to " + knobs.getProperty(p.getKey()));
         }
       }
+    } else {
+      System.out.println("\t - AutoConf: There is no knobs to tune. Nothing to do.");
     }
   }
 
-  public void showConfiguration() {
-    System.out.println("\n");
-    System.out.println(" *** JobName :: " + getJobName());
-    System.out.println(" *** JarFile :: " + getJarFile());
-
-    HashMap<String, Long> m = getInputData();
-    Iterator i = m.entrySet().iterator();
-    while (i.hasNext()) {
-      Map.Entry inputData = (Map.Entry) i.next();
-      System.out.println(" *** Input data  :: " + inputData.getValue() + " " + inputData.getKey());
-    }
-
-    System.out.println(" *** Total input size :: " + getInputSize());
-
-    if (getMapperName() != null)
-      System.out.println(" *** Mapper  :: " + getMapperName().getName());
-    if (getReducerName() != null)
-      System.out.println(" *** Reducer :: " + getReducerName().getName());
-
-    /* show the tuning knobs */
-    if (getKnobs().size() > 0) {
-      System.out.println(" *** Knobs   :: " + getKnobs().size());
+  /**
+   * Print all the Tuning Knobs from this.knobs
+   */
+  public void show() {
+    if (getTuningKnobs().size() > 0) {
+      System.out.println(" *** Knobs   :: " + getTuningKnobs().size());
       Enumeration e = knobs.propertyNames();
       while (e.hasMoreElements()) {
         String key = (String) e.nextElement();
@@ -230,90 +113,5 @@ public class TuningKnobs implements Serializable {
       }
       System.out.println("\n");
     }
-  }
-
-  public void exportXml() throws ParserConfigurationException, TransformerException {
-
-    DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-    DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-
-    Document doc = docBuilder.newDocument();
-    Element rootElement = doc.createElement("jobs");
-    doc.appendChild(rootElement);
-
-    Element job = doc.createElement("job");
-    rootElement.appendChild(job);
-    job.setAttribute("name", getJobName());
-
-    Element jar = doc.createElement("jar");
-    jar.appendChild(doc.createTextNode(getJarFile()));
-    job.appendChild(jar);
-
-    if (getMapperName() != null) {
-      Element map = doc.createElement("mapper");
-      map.setAttribute("map", getMapperName().getName());
-
-      for (Method m : getMapperName().getMethods()) {
-        Element mapMethod = doc.createElement("method");
-        mapMethod.appendChild(doc.createTextNode(m.getName()));
-        map.appendChild(mapMethod);
-      }
-      job.appendChild(map);
-    }
-
-    if (getReducerName() != null){
-      Element reduce = doc.createElement("reducer");
-      reduce.setAttribute("reduce", getReducerName().getName());
-
-      for (Method r : getReducerName().getMethods()) {
-        Element reduceMethod = doc.createElement("method");
-        reduceMethod.appendChild(doc.createTextNode(r.getName()));
-        reduce.appendChild(reduceMethod);
-      }
-      job.appendChild(reduce);
-    }
-
-    Element input = doc.createElement("input");
-    HashMap<String, Long> m = getInputData();
-    input.setAttribute("elements", String.valueOf(m.size()));
-    if (m.size() > 0 ) {
-      input.setAttribute("total_size", String.valueOf(getInputSize()));
-      Iterator i = m.entrySet().iterator();
-      while (i.hasNext()) {
-        Map.Entry inputData = (Map.Entry) i.next();
-        Element data = doc.createElement("data");
-        data.setAttribute("path", String.valueOf(inputData.getKey()));
-        data.setAttribute("size", String.valueOf(inputData.getValue()));
-        input.appendChild(data);
-      }
-    }
-    job.appendChild(input);
-
-    Element tKnobs = doc.createElement("knobs");
-    tKnobs.setAttribute("elements", String.valueOf(getKnobs().size()));
-    if (getKnobs().size() > 0) {
-      Enumeration e = knobs.propertyNames();
-      while (e.hasMoreElements()) {
-        String key = (String) e.nextElement();
-        Element knob = doc.createElement("knob");
-        knob.setAttribute("tuning_knob", key);
-        knob.setAttribute("setup_value",knobs.getProperty(key));
-
-        tKnobs.appendChild(knob);
-      }
-    }
-    job.appendChild(tKnobs);
-
-    TransformerFactory transformerFactory = TransformerFactory.newInstance();
-    Transformer transformer = transformerFactory.newTransformer();
-    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-
-    DOMSource source = new DOMSource(doc);
-    // write the content into xml file
-    //StreamResult result = new StreamResult(new File("/tmp/"));
-    // Output to console for testing
-    StreamResult result = new StreamResult(System.out);
-    transformer.transform(source, result);
   }
 }
